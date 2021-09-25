90bdf0410f54276b994fa904d55168107b4b65f2
mbelgaid <mohammed-chakib.belgaid@inria.fr>
2021-09-25 13:43:15 +0000

    add : dynamic programming

--- roller-coaster.ts
+++ roller-coaster.ts
@@ -3,24 +3,46 @@ import { RollerCoaster } from "./roller-coaster.d"
 const fs = require('fs');
 const readline = require('readline');
 
+interface Earning {
+    numberofplayers: number
+    size: number
+}
 
 function executesimulation(rollercoaster: RollerCoaster) {
     let ride = 0
-    let begin = 0;
+    let begin = 0, end;
     let totalbenifits = 0
     let numberofplayers
     // console.log(rollercoaster)
+    var old_benifits: { [begin: number]: Earning } = {}
     while (ride < rollercoaster.numberOfRidesPerDay) {
+        // console.log(begin, ' to', totalbenifits)
         numberofplayers = 0
-        while (numberofplayers <= rollercoaster.limitePersonsPerRide) {
-            numberofplayers = numberofplayers + rollercoaster.personsPerGroup[begin]
-            begin = (begin + 1) % rollercoaster.numberOfGroups
-            // console.log("bgin : " + begin)
-            // console.log('numberofplayers ', numberofplayers)
+        if (old_benifits[begin]) {
+
+            // console.log("begin : " + begin + " benifits " + old_benifits[begin].numberofplayers)
+            begin = (begin + old_benifits[begin].size) % rollercoaster.numberOfGroups
+            totalbenifits += old_benifits[begin].numberofplayers
+
         }
-        begin = (begin - 1 + rollercoaster.numberOfGroups) % rollercoaster.numberOfGroups // undo the last changes of the loop 
-        numberofplayers -= rollercoaster.personsPerGroup[begin];
-        totalbenifits += numberofplayers
+        else {
+            end = begin
+            while (numberofplayers <= rollercoaster.limitePersonsPerRide) {
+                numberofplayers = numberofplayers + rollercoaster.personsPerGroup[end]
+                end = (end + 1) % rollercoaster.numberOfGroups
+                // console.log("bgin : " + begin)
+                // console.log('numberofplayers ', numberofplayers)
+            }
+            end = (end - 1 + rollercoaster.numberOfGroups) % rollercoaster.numberOfGroups // undo the last changes of the loop 
+            numberofplayers -= rollercoaster.personsPerGroup[end];
+            totalbenifits += numberofplayers
+            /// adding the result to thje memory 
+            old_benifits[begin] = {
+                numberofplayers: numberofplayers,
+                size: (end - begin + rollercoaster.numberOfGroups) % rollercoaster.numberOfGroups
+            }
+            begin = end
+        }
         // if (begin === 0) {
         //     console.log("we came back ")
         //     let remainingoops = rollercoaster.numberOfRidesPerDay / ride - 1
@@ -77,9 +99,9 @@ async function parsesample(path: string) {
 // parsesample("./samples/1_simple_case.txt")
 // parsesample("./samples/2_1000_groups_of_few_people.txt")
 // parsesample("./samples/3_the_same_groups_go_on_the_ride_several_times_during_the_day.txt")
-// parsesample("./samples/4_all_the_people_get_on_the_roller_coaster_at_least_once.txt")
+// parsesample("./samples/4_all_the_people_get_on_the_roller_coaster_at_least_once.txt") // error 
 // parsesample("./samples/5_high_earnings_during_the_day.txt")
-// parsesample("./samples/6_works_with_a_large_dataset.txt")
+// parsesample("./samples/6_works_with_a_large_dataset.txt") // error 
 parsesample("./samples/7.hard.txt")
 // parsesample("./samples/8.harder.txt") // DIDNT work
 // try {
